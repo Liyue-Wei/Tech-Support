b8ccd2b2a8543ba958b7c58dae87c85536c080bb
Liyue-Wei <126851525+Liyue-Wei@users.noreply.github.com>
2023-03-24 14:03:08 +0800

    web_control.py

---
+++ web_control.py
@@ -0,0 +1,14 @@
+import os
+from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
+
+os.popen('start chrome --remote-debugging-port=9527 --user-data-dir="C:\chromedriver_win32"')
+opt = Options()
+opt.add_experimental_option("debuggerAddress", "127.0.0.1:9527")
+driver = webdriver.Chrome(options=opt)
+
+'''
+1. chromedriver放在C槽，並且設置全域變數
+2. 使用開發者模式開啟Chrome才不會被Google擋登入
+3. 定期更新chromedriver，因為Chrome三不五時就會自動更新
+'''
