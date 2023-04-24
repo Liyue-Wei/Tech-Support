197058a88eb63915571ff98f2b0784f81475f403
Zhe Wei Chang <126851525+Liyue-Wei@users.noreply.github.com>
2023-04-24 19:02:04 +0800

    Update prograssbar_test.py

--- prograssbar_test.py
+++ prograssbar_test.py
@@ -1,13 +1,13 @@
 import time
 from tqdm import tqdm, trange
 
-for i in trange(100):
+for i in trange(50):
     time.sleep(0.05)
 
 for i in tqdm(range(100), desc='Processing'):
     time.sleep(0.05)
 
-dic = ['a', 'b', 'c', 'd', 'e']
+dic = ['a', 'b', 'c', 'd', 'e', 'f']
 pbar = tqdm(dic)
 for i in pbar:
     pbar.set_description('Processing '+i)
