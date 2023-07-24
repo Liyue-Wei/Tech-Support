ca53d8acb2cdd425317201b48e95db9756fb4715
Zhe Wei Chang <126851525+Liyue-Wei@users.noreply.github.com>
2023-07-24 13:05:10 +0800

    Update send_email.py

--- send_email.py
+++ send_email.py
@@ -14,3 +14,9 @@ def send(master, slave, title, content, smtp, tcp, password):
     server.login(master, password)
     server.send_message(msg)
     server.close()
+
+'''
+1. Gmail的SMTP伺服器為 smtp.gmail.com
+2. Gmail的TCP埠為 465
+3. 以Gmail進行郵件發送，須先取得應用程式密碼
+'''
