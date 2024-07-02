19c2fe9d8e91966604176a908fa0cece1cf256c8
Zhe Wei Chang <126851525+Liyue-Wei@users.noreply.github.com>
2024-07-03 06:19:53 +0800

    Update camera.py

--- camera.py
+++ camera.py
@@ -1,4 +1,5 @@
 import cv2
+import time
 
 cam = cv2.VideoCapture(0)
 width = int(cam.get(cv2.CAP_PROP_FRAME_WIDTH))  
@@ -12,10 +13,15 @@ if not cam.isOpened():
 
 while True:
     ret, img = cam.read()
+    key = cv2.waitKey(1)
     if not ret:
         print("Camera is not available")
         break
     
     cv2.imshow('Camera', img)    
-    if cv2.waitKey(1) == 27:    #ESC
-        break             
+    if key == 27:    #ESC
+        break  
+
+    elif key == 13:    #Enter
+        file_name = ("ScreenShot "+time.ctime(time.time())+ ".jpeg")
+        image = cv2.imwrite(file_name, img)             
