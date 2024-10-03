d4962a33a5f85001a8935cc16733df05fe5f2b31
Zhe Wei Chang <126851525+Liyue-Wei@users.noreply.github.com>
2024-10-03 14:48:49 +0800

    Update variable_type_check.cpp

--- variable_type_check.cpp
+++ variable_type_check.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
     int i;
     cin >> i;
-    cout << i  << " " << typeid(i).name() << '\n';
+    cout << i << " " << typeid(i).name() << '\n';
     double n = (double)i;
     cout << n << " " << typeid(n).name() << '\n';
 
